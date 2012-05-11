09d3a76cd1ab6329364cf795e6f91c91a546a26d
jagarcia_1º_Commit <jagarcia@106-07.info2>
2012-05-11 09:51:51 +0200

    jagarcia_1º_Commit <jagarcia@106-07.info2>
    Fecha 11_05_2012

---
+++ PROJECT_GIT/src/Hola_mundo.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+public class Hola_mundo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola_Gol_Norte.\nHola Gol Sur.");
+		System.out.println("Viva el Betis.\n" +
+				"Adios a los Accionistas D.Manuel Ruiz y de Lopera .");
+	}
+
+}
